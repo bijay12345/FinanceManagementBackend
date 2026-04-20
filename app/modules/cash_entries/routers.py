f3bebd2912e14f9d7fec06c4b4f62c985d3b0c75
bijay12345 <sbijay433@gmail.com>
2026-04-20 23:17:24 +0530

    feat: completed CRUD for cash entries

--- app/modules/cash_entries/routers.py
+++ app/modules/cash_entries/routers.py
@@ -20,3 +20,23 @@ async def create_cash_entries(
 async def list_cash_entries(db: AsyncSession = Depends(get_db)):
     data = await service.list_cash_entries(db)
     return data
+
+
+@router.get("/{id}", response_model=CashEntriesResponse)
+async def get_cash_entry(id: int, db: AsyncSession = Depends(get_db)):
+    entry = await service.get_cash_entry(id, db)
+    return entry
+
+
+@router.put("/{id}", response_model=CashEntriesResponse)
+async def update_cash_entry(
+    id: int, data: CashEntriesUpdate, db: AsyncSession = Depends(get_db)
+):
+    updated_data = await service.update_cash_entry(id, data, db)
+    return updated_data
+
+
+@router.delete("/{id}")
+async def delete_cash_entry(id: int, db: AsyncSession = Depends(get_db)):
+    await service.delete_cash_entry(id, db)
+    return {"status": "deleted"}
--- app/modules/cash_entries/service.py
+++ app/modules/cash_entries/service.py
@@ -1,7 +1,8 @@
-from .schemas import CashEntriesCreate
+from .schemas import CashEntriesCreate, CashEntriesUpdate
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
 from .models import CashEntries
+from fastapi import Response
 
 
 async def create_cash_entry(db: AsyncSession, data: CashEntriesCreate):
@@ -20,3 +21,30 @@ async def list_cash_entries(db: AsyncSession, skip: int = 0, limit: int = 10):
         .order_by(CashEntries.created_at.desc())
     )
     return data.scalars().all()
+
+
+async def get_cash_entry(id: int, db: AsyncSession):
+    data = await db.execute(select(CashEntries).where(CashEntries.id == id))
+    return data.scalar_one_or_none()
+
+
+async def update_cash_entry(id: int, data: CashEntriesUpdate, db: AsyncSession):
+    record = await db.get(CashEntries, id)
+    if not record:
+        return Response("Item not found")
+
+    update_record = data.model_dump(exclude_unset=True)
+    for key, value in update_record.items():
+        setattr(record, key, value)
+    await db.commit()
+    await db.refresh(record)
+    return record
+
+
+async def delete_cash_entry(id: int, db: AsyncSession):
+    record = await db.get(CashEntries, id)
+    if not record:
+        return Response("Item not found")
+    await db.delete(record)
+    await db.commit()
+    return id
